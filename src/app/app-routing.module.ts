f03b46957e42376377134d32eb3ad21e730a99fd
Angelo Bonini <abonini2000@gmail.com>
2024-04-23 17:10:57 -0300

    Revert "Products List Page"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
-// src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductListComponent } from './products/products-list/products-list.component';
 
-const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'products' },
-  { path: 'products', component: ProductListComponent, title: 'Product List' }
-  // Other routes can be added here
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
